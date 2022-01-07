a752ae1543a01f9558bc04889e76a003db03e02b
berk2s <berktopcu@outlook.com>
2022-01-07 09:22:32 +0300

    detaıl page missing field

--- js/pages/detail.js
+++ js/pages/detail.js
@@ -1,9 +1,15 @@
 import { getProductById } from "../services/api";
 import { renderProductDetail } from "../helpers/render-product-details";
 
+/**
+ * Initializes product detail pages with
+ * product id that is given in the URL
+ */
 export const initializeProductDetail = async function() {
     try {
         if (document.querySelector(".js-product-title")) {
+            const path = document.location.pathname;
+
             if (!path ||
                 path.trim() === '' ||
                 path.split('/')[0].trim() !== 'details') {
